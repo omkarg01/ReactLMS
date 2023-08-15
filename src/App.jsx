1362d8a3942b75134ab5be0f1df99cd17463d1c5
omkarg01 <omkargujja01@gmail.com>
2023-08-15 19:42:54 +0530

    Event Handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import ChildComponent from './components/ChildComponent';
 
 const App = () => {
-
-  const message = "Hello from Parent Component"
+  
 
   return (
-    <div>
+    <div> 
       This is App Component
-      <ChildComponent greeting={message}></ChildComponent>
     </div>
   )
 }
