db7be2e9ea3af299a99205949abb2b007ab6e227
omkarg01 <omkargujja01@gmail.com>
2023-08-17 19:14:42 +0530

    useState

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Counter = () => {
-  let count = 0;
+  const [count, setCount] = useState(0)
+  // let count = 0;
 
+  const incrementHandler =()=>{
+    // count += 1;
+    setCount(count + 1)
+    console.log("count :", count);
+  }
   return (
     <div>
       <p>Count : {count}</p>
+      <button onClick={incrementHandler}>Increment</button>
     </div>
   );
 };
