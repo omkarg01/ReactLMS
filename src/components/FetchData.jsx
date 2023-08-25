7776e79c045a0b3c58a8f48ad84ae6789ae47934
omkarg01 <omkargujja01@gmail.com>
2023-08-25 19:30:55 +0530

    useEffect 1 example

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -1,19 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import TodoList from "./TodoList";
 
 const TodoApp = () => {
   const [taskList, setTaskList] = useState([]);
 
   const fetchData = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const response = await fetch("https://jsonplaceholder.typicode.com/comments");
     const data = await response.json();
     setTaskList(data);
     console.log(data);
   };
 
+  useEffect(()=>{
+    fetchData()
+  },[])
+
   return (
     <div>
-      <h1 style={{marginLeft : '15%'}}>Todo List</h1>
+      {console.log("Hello")}
+      <h1 style={{ marginLeft: "15%" }}>Todo List</h1>
       <TodoList data={taskList}></TodoList>
     </div>
   );
