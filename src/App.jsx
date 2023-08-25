6699b527e6bac88a4c8e84f2dee5938e5f820e84
omkarg01 <omkargujja01@gmail.com>
2023-08-26 04:07:05 +0530

    Dropdown example

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import TodoApp from './components/FetchData';
+import DropdownComponent from './components/DropDownComponent';
 
 const App = () => {
 
   return (
     <div> 
-      <TodoApp></TodoApp>
+      {/* <TodoApp></TodoApp> */}
+      <DropdownComponent></DropdownComponent>
     </div>
   )
 }
