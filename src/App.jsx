0c78ce9dbf3ae568dfe567877baa629c8e13cbee
omkarg01 <omkargujja01@gmail.com>
2023-08-29 03:29:08 +0530

    Routing branch created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Header from './components/Header';
 
 const App = () => {
 
   return (
     <div>
       This is App Component
-      <Header></Header>
     </div>
   )
 }
