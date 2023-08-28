14c0d84e29d72c24609cce0e37c03ff4f2bdb576
omkarg01 <omkargujja01@gmail.com>
2023-08-29 05:03:15 +0530

    Revert "Merge branch"
    This reverts commit 335002f08ac642d92b4a048ba556dfb3b872f82e, reversing
    changes made to ed43332e15ffd2f40099b54b3d111147d7eb48b4.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import Product from "./components/Product";
+
+// http://example.com/ - Home
+// http://example.com/products - Products
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Product /> },
+]);
 
 const App = () => {
+  return <RouterProvider router={router} />;
+};
 
   return (
     <div>
