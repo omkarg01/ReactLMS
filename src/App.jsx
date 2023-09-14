c3f5bdcdc88a8c385b6133d935b64a51a6585aa1
omkarg01 <omkargujja01@gmail.com>
2023-09-15 03:41:43 +0530

    redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Counter from './components/Counter';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const App = () => {
-
   return (
-    <div>
+    <Provider store={store}>
       <Counter></Counter>
-    </div>
+    </Provider>
   )
 }
 
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "../counterSlice";
 
 const Counter = () => {
+
+  const count = useSelector((state)=>(state.counter.value))
+  const dispatch = useDispatch()
+  
   return (
     <div>
-      <button>-</button>
-      <span style={{ margin: "10px" }}>5</span>
-      <button>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <span style={{ margin: "10px" }}>{count}</span>
+      <button onClick={() => dispatch(increment())}>+</button>
     </div>
   );
 };
